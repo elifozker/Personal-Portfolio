81af3cd1488a6a6a09fa508d9d302278d1066697
elifozker <elif_ozker@hotmail.com>
2023-08-08 16:43:31 +0300

    image responsive addded

--- app/(user)/home/page.jsx
+++ app/(user)/home/page.jsx
@@ -5,19 +5,19 @@ import Marquee from "react-fast-marquee";
 function Home() {
   return (
     <section id="home" className="w-screen h-screen bg-[#f4a35a] relative">
-
-        <div className="flex flex-col items-center justify-center h-screen ">
-          <img
-            src="../elifozker.png"
-            alt="Image"
-            className="md:w-[28rem] md:h-[28rem] mb-16 z-10  transition-all duration-300 hover:scale-[1.12]"
-          ></img>
-          <p className="text-5xl font-medium">WELCOME!</p>
-        </div>
-        <Marquee className="absolute text-7xl top-[-400px] z-0">elo elo elo elo elo elo elo elo elo elo elo elo elo elo elo elo elo
-          elo elo 
-        </Marquee>
-        
+      <div className="flex flex-col items-center justify-center h-screen ">
+        <img
+          src="../elifozker.png"
+          alt="Image"
+          className="md:w-[28rem] md:h-[28rem] mb-16 z-10 transition-all duration-300 hover:scale-[1.12]"
+          style={{ maxWidth: "80%", maxHeight: "80vw" }} // Mobil için resmi küçültmek için stil ekledik
+        ></img>
+        <p className="text-5xl font-medium">WELCOME!</p>
+      </div>
+      <Marquee className="absolute sm:text-8xl md:text-7xl top-[-400px] z-0">
+         elo elo elo elo elo elo elo elo elo elo elo elo elo elo elo elo elo elo elo
+        elo elo
+      </Marquee>
     </section>
   );
 }
