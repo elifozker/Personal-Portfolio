39eecd9bab7b6a9496146ba2e5e064ffdd8233d4
Elif Özker <111710264+elifozker@users.noreply.github.com>
2023-09-01 21:33:01 +0300

    about commit

--- app/(user)/about/page.jsx
+++ app/(user)/about/page.jsx
@@ -3,25 +3,23 @@ import React from "react";
 function About() {
   return (
     <div id="about" className="w-auto h-screen bg-[#F9F9F9]">
-      <div className="mx-auto max-w-7xl pt-44 sm:container sm:px-12">
-        <section className="mx-auto justify-items-center">
-          <p className="sm:text-lg md:text-3xl md:leading-loose">
-            <em>Hello</em> I am Elif,
-            a 4th-year Computer Engineering
-            student at Dokuz Eylül University.
-            Junior full-stack web developer based in Izmir. I have
-            a strong passion for web development and love exploring its various
-            facets. I have a passion for frontend development. Creating visually
-            appealing and user-friendly interfaces excites me. I love combining
-            my creativity with technical skills to bring ideas to life on the
-            web. Exploring new design trends and modern frontend technologies
-            keeps me inspired and motivated.
-          </p>
-        </section>
+      <div className="container mx-auto pt-44 sm:px-32 flex flex-col items-center justify-center">
+
+        <p className="sm:text-lg md:text-3xl md:leading-loose">
+          <em>Hello</em> I am Elif,
+          a 4th-year Computer Engineering
+          student at Dokuz Eylül University.
+          Junior full-stack web developer based in Izmir. I have
+          a strong passion for web development and love exploring its various
+          facets. I have a passion for frontend development. Creating visually
+          appealing and user-friendly interfaces excites me. I love combining
+          my creativity with technical skills to bring ideas to life on the
+          web. Exploring new design trends and modern frontend technologies
+          keeps me inspired and motivated.
+        </p>
+
       </div>
     </div>
-
-
   );
 }
 
